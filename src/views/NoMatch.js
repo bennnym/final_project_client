4936487944051bd58b80f9645a039f2cf3905c47
Ben Muller <ben.k.muller@gmail.com>
2019-06-10 14:24:49 +1000

    deploy

--- src/views/NoMatch.js
+++ src/views/NoMatch.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NoMatch = () => {
   return(
-    <h1>NoMatchGoHome</h1>
+    <h1>No Match Go <Link to="/">Home</Link></h1>
+
   );
 }
 
