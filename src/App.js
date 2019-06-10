82be9d8ed394b733fdc8c80f7ed10eee19cd8a56
Ben Muller <ben.k.muller@gmail.com>
2019-06-10 16:37:50 +1000

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from '../src/reducers/reducers'
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "../src/reducers/reducers";
 // Views
 import Home from "./views/Home";
 import NoMatch from "./views/NoMatch";
 import Profile from "./views/Profile";
 import Auctions from "./views/Auctions";
-import MyAcc from "./views/MyAcc"
+import MyAcc from "./views/MyAcc";
 //Font Awesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -24,66 +24,52 @@ import {
 	faHeart,
 	faSnowman,
 	faInbox,
-	faDice
+	faDice,
 } from "@fortawesome/free-solid-svg-icons";
 
-
-
-
-library.add(faUserGraduate, faBuilding, faSignInAlt, faUserPlus, faSpinner, faSearch, faCheckCircle, faTimesCircle, faHeart, faSnowman, faInbox, faDice);
+library.add(
+	faUserGraduate,
+	faBuilding,
+	faSignInAlt,
+	faUserPlus,
+	faSpinner,
+	faSearch,
+	faCheckCircle,
+	faTimesCircle,
+	faHeart,
+	faSnowman,
+	faInbox,
+	faDice
+);
 
 function App() {
-
 	const store = createStore(
-		reducer, /* preloadedState, */
+		reducer /* preloadedState, */,
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	);
 
 	return (
 		<React.Fragment>
-		<Provider store={store}>
-			<Router>
-				<Switch>
-					<Route
-						exact
-						path='/'
-						render={props => (
-							<Home
-								{...props}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/profile/:id'
-						render={props => (
-							<Profile
-								{...props}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/auctions'
-						render={props => (
-							<Auctions
-								{...props}
-							/>
-						)}
-					/>
+			<Provider store={store}>
+				<Router>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						)} />
 						<Route
 							exact
-							path='/myacc'
-							render={props => (
-								<MyAcc
-									{...props}
-								/>
-							)}
+							path='/profile/:id'
+							render={props => <Profile {...props} />}
 						/>
-					<Route component={NoMatch} />
-				</Switch>
-			</Router>
-		</Provider>
+						<Route
+							exact
+							path='/auctions'
+							render={props => <Auctions {...props} />}
+						/>
+						<Route exact path='/myacc' render={props => <MyAcc {...props} />} />
+						<Route component={NoMatch} />
+					</Switch>
+				</Router>
+			</Provider>
 		</React.Fragment>
 	);
 }
